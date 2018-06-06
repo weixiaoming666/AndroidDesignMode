22f46c9cc9386cc9b7477d069645c94f2e68b981
weixiaoming666 <307357947@qq.com>
2018-06-06 11:47:22 +0800

    Revert "测试"
    This reverts commit c4b69b5f2f17c1464a75ee78f2c45e0d208563f5.

--- app/src/main/java/com/example/administrator/androiddesignmode/observer/ObserverAty.java
+++ app/src/main/java/com/example/administrator/androiddesignmode/observer/ObserverAty.java
@@ -1,10 +1,8 @@
 package com.example.administrator.androiddesignmode.observer;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.example.administrator.androiddesignmode.MainActivity;
 import com.example.administrator.androiddesignmode.R;
 //观察者模式
 public class ObserverAty extends AppCompatActivity {
@@ -14,7 +12,5 @@ public class ObserverAty extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_observer_aty);
         setTitle("观察者模式哦");
-
-        startActivity(new Intent(this, MainActivity.class));
     }
 }
