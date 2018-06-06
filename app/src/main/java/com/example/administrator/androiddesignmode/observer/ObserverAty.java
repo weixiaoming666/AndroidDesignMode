c4b69b5f2f17c1464a75ee78f2c45e0d208563f5
weixiaoming666 <307357947@qq.com>
2018-06-06 11:43:53 +0800

    测试

--- app/src/main/java/com/example/administrator/androiddesignmode/observer/ObserverAty.java
+++ app/src/main/java/com/example/administrator/androiddesignmode/observer/ObserverAty.java
@@ -1,8 +1,10 @@
 package com.example.administrator.androiddesignmode.observer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.administrator.androiddesignmode.MainActivity;
 import com.example.administrator.androiddesignmode.R;
 //观察者模式
 public class ObserverAty extends AppCompatActivity {
@@ -12,5 +14,7 @@ public class ObserverAty extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_observer_aty);
         setTitle("观察者模式哦");
+
+        startActivity(new Intent(this, MainActivity.class));
     }
 }
